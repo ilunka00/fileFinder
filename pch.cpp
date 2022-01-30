91fad9403e5b306352703d6b7219c01d5a0561b9
[illia] <[ilunka00@gmail.com]>
2022-01-30 14:02:26 +0200

    not compiling using threads

--- pch.cpp
+++ pch.cpp
@@ -1,44 +1,13 @@
 ﻿#include "pch.h"
 
-std::string TestClass::fileFind(std::string name)
+#define MAX_THREAD 8
+
+
+
+void TestClass::fileFind(std::string& name,std::string& path)
 {
-    std::string result = "C:\\";
-    WIN32_FIND_DATA file;
-    HANDLE search_handle = FindFirstFile(L"C:\\*", &file);
-    if (search_handle != INVALID_HANDLE_VALUE)
-    {
-        do
-        {
-            result = "C:\\";
-
-            std::wstring ws(file.cFileName);
-            std::string test(ws.begin(), ws.end());
-
-            if (test == "."||test=="..") continue;
-
-            if (name==test) {
-                return result + test;
-            }
-
-            std::string temp = result;
-            result += test;
-            result += "\\";
-            
-            std::string tempResult = fileFind(name, result);
-            if (tempResult.find(name) != -1) return tempResult;
-
-            result = temp;
-
-        } while (FindNextFile(search_handle, &file));
-        FindClose(search_handle);
-        
-
-    }
-    return "";
-}
-
-std::string TestClass::fileFind(std::string name, std::string path)
-{
+    std::thread threads[8];
+    found = false;
     std::string result = path;
     result += "*";
     WIN32_FIND_DATA file;
@@ -46,35 +15,100 @@ std::string TestClass::fileFind(std::string name, std::string path)
     std::wstring widestr = std::wstring(result.begin(), result.end());
     const wchar_t* wstrResult = widestr.c_str();
 
+
     HANDLE search_handle = FindFirstFile(wstrResult, &file);
     if (search_handle != INVALID_HANDLE_VALUE)
     {
         do
         {
+
             std::wstring ws(file.cFileName);
             std::string test(ws.begin(), ws.end());
-
             if (test == "." || test == "..") continue;
+            result = path;
 
-            if (name == test) {
-                return result + test;
+
+            if (name==test) {
+                found = true;
+                threads_started = 0;
+                resultPath = result + test;
+                return;
             }
 
             std::string temp = path;
             path += test;
-            path += "\\";
-
-            std::string tempResult = fileFind(name, path);
-
+            path += "\\";            
+            if (threads_started < MAX_THREAD) {
+                threads[threads_started] = std::thread(&TestClass::fileFind, std::ref(name), std::ref(path));
+                threads[threads_started].detach();
+                threads_started++;
+            }
+            else {
+                threads[0].join();
+                threads[1].join();
+                threads[2].join();
+                threads[3].join();
+                threads[4].join();
+                threads[5].join();
+                threads[6].join();
+                threads[7].join();
+                threads_started = 0;
+                threads[threads_started] = std::thread(&TestClass::fileFind, std::ref(name), std::ref(path));
+                threads[threads_started].detach();
+                threads_started++;
+            }
             
-            if (tempResult.find(name) != -1) return tempResult;
+            
             path = temp;
+
         } while (FindNextFile(search_handle, &file));
         FindClose(search_handle);
+        
 
     }
-    return "";
+    return;
 }
+
+//std::string TestClass::fileFind(std::string name, std::string path)
+//{
+//    if (found) return;
+//    std::string result = path;
+//    result += "*";
+//    WIN32_FIND_DATA file;
+//
+//    std::wstring widestr = std::wstring(result.begin(), result.end());
+//    const wchar_t* wstrResult = widestr.c_str();
+//
+//    HANDLE search_handle = FindFirstFile(wstrResult, &file);
+//    if (search_handle != INVALID_HANDLE_VALUE)
+//    {
+//        do
+//        {
+//            std::wstring ws(file.cFileName);
+//            std::string test(ws.begin(), ws.end());
+//
+//            if (test == "." || test == "..") continue;
+//
+//            if (name == test) {
+//                found = true;
+//                return result + test;
+//            }
+//
+//            std::string temp = path;
+//            path += test;
+//            path += "\\";
+//
+//            std::string tempResult = fileFind(name, path);
+//
+//            
+//            if (tempResult.find(name) != -1) return tempResult;
+//            path = temp;
+//        } while (FindNextFile(search_handle, &file));
+//        FindClose(search_handle);
+//
+//    }
+//    return "";
+//}
 
 void TestClass::testing() {
     WIN32_FIND_DATA file;
--- pch.h
+++ pch.h
@@ -1,21 +1,19 @@
-﻿// pch.h: это предварительно скомпилированный заголовочный файл.
-// Перечисленные ниже файлы компилируются только один раз, что ускоряет последующие сборки.
-// Это также влияет на работу IntelliSense, включая многие функции просмотра и завершения кода.
-// Однако изменение любого из приведенных здесь файлов между операциями сборки приведет к повторной компиляции всех(!) этих файлов.
-// Не добавляйте сюда файлы, которые планируете часто изменять, так как в этом случае выигрыша в производительности не будет.
-
-#ifndef PCH_H
+﻿#ifndef PCH_H
 #define PCH_H
 
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <thread>
 
 class TestClass
 {
+private:
+	bool found;
+	std::string resultPath;
+	int threads_started=0;
 public:
-	std::string fileFind(std::string);
-	std::string fileFind(std::string name, std::string path);
+	void fileFind(std::string& name, std::string& path);
 	void testing();
 };
 
